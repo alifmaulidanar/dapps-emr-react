0c0497770aaa1427620da8595eae165e048ed226
alifmaulidanar <alifmaulidanr@gmail.com>
2023-12-03 19:43:35 +0700

    Fixing appointment modal

--- frontend/src/components/Buttons/MakeAppointment.jsx
+++ frontend/src/components/Buttons/MakeAppointment.jsx
@@ -5,8 +5,10 @@ import "flatpickr/dist/flatpickr.min.css";
 // import Datepicker from "../Datepicker";
 // import Datepicker from "../Datepicker";
 
-export default function RegisterPatientButton({buttonText}) {
+export default function RegisterPatientButton({ buttonText }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentStep, setCurrentStep] = useState(0);
+  const [selectedDate, setSelectedDate] = useState(null);
   const flatpickrRef = useRef(null);
 
   useEffect(() => {
@@ -16,9 +18,13 @@ export default function RegisterPatientButton({buttonText}) {
         dateFormat: "d/m/Y",
         minDate: "today",
         maxDate: new Date().fp_incr(30),
+        defaultDate: selectedDate, // Set default date
+        onChange: (selectedDates, dateStr, instance) => {
+          setSelectedDate(dateStr);
+        },
       });
     }
-  }, [isModalOpen]);
+  }, [isModalOpen, currentStep, selectedDate]);
 
   const showModal = () => {
     setIsModalOpen(true);
@@ -32,8 +38,29 @@ export default function RegisterPatientButton({buttonText}) {
     setIsModalOpen(false);
   };
 
-  const dateFormat = "DD/MM/YYYY";
-  const customFormat = (value) => `${value.format(dateFormat)}`;
+  const handleNext = () => {
+    setCurrentStep(currentStep + 1);
+  };
+
+  const handleBack = () => {
+    setCurrentStep(currentStep - 1);
+  };
+
+  const renderStepContent = (step) => {
+    switch (step) {
+      case 0:
+        return;
+      case 1:
+        return;
+      case 2:
+        return;
+      default:
+        return null;
+    }
+  };
+
+  // const dateFormat = "DD/MM/YYYY";
+  // const customFormat = (value) => `${value.format(dateFormat)}`;
 
   const items = [
     { title: "Pilih Jadwal Dokter" },
@@ -74,112 +101,204 @@ export default function RegisterPatientButton({buttonText}) {
         </h2>
         <div className="grid justify-center w-2/3 grid-cols-2 mx-auto">
           <Steps
-            current={0}
+            current={currentStep}
             labelPlacement="vertical"
             items={items}
             className="w-full col-span-2 pt-4"
           />
         </div>
         <form className="p-8">
-          <div className="grid">
-            <div className="mb-6 text-lg font-medium text-gray-900">
-              Pilih Jadwal Dokter
-              <hr className="h-px bg-gray-700 border-0"></hr>
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="gender"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Pilih Rumah Sakit
-              </label>
-              <select
-                id="gender"
-                className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                required
-              >
-                <option>Pilih Rumah Sakit</option>
-                <option value="0">Jakarta</option>
-                <option value="1">Bekasi</option>
-                <option value="2">Tangerang</option>
-              </select>
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="rs"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Pilih Kategori Dokter
-              </label>
-              <select
-                id="rs"
-                className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                required
-              >
-                <option>Pilih Kategori Dokter</option>
-                <option value="0">Umum</option>
-              </select>
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="category"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Pilih Dokter
-              </label>
-              <select
-                id="category"
-                className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                required
-              >
-                <option>Pilih Dokter</option>
-                <option value="0">Jakarta</option>
-                <option value="1">Bekasi</option>
-                <option value="2">Tangerang</option>
-              </select>
-            </div>
-            <div className="mb-6">
-              <p>
-                Dokter{" "}
-                <span className="font-medium text-gray-900">[NAMA DOKTER]</span>{" "}
-                hanya tersedia pada hari [HARI], [HARI], dan [HARI]
-              </p>
-            </div>
-            <div className="grid grid-cols-2 mb-6">
-              <div>
+          {renderStepContent(currentStep)}
+          {currentStep === 0 && (
+            <div className="grid">
+              <div className="mb-6 text-lg font-medium text-gray-900">
+                Pilih Jadwal Dokter
+                <hr className="h-px bg-gray-700 border-0"></hr>
+              </div>
+              <div className="mb-6">
                 <label
-                  htmlFor="date"
+                  htmlFor="gender"
                   className="block mb-2 text-sm font-medium text-gray-900"
                 >
-                  Pilih Tanggal
+                  Pilih Rumah Sakit
                 </label>
-                <div ref={flatpickrRef}></div>
+                <select
+                  id="gender"
+                  className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  required
+                >
+                  <option>Pilih Rumah Sakit</option>
+                  <option value="0">Jakarta</option>
+                  <option value="1">Bekasi</option>
+                  <option value="2">Tangerang</option>
+                </select>
               </div>
-              <div>
+              <div className="mb-6">
                 <label
-                  htmlFor="time"
+                  htmlFor="rs"
                   className="block mb-2 text-sm font-medium text-gray-900"
                 >
-                  Pilih Waktu
+                  Pilih Kategori Dokter
                 </label>
+                <select
+                  id="rs"
+                  className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  required
+                >
+                  <option>Pilih Kategori Dokter</option>
+                  <option value="0">Umum</option>
+                </select>
+              </div>
+              <div className="mb-6">
+                <label
+                  htmlFor="category"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Pilih Dokter
+                </label>
+                <select
+                  id="category"
+                  className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  required
+                >
+                  <option>Pilih Dokter</option>
+                  <option value="0">Jakarta</option>
+                  <option value="1">Bekasi</option>
+                  <option value="2">Tangerang</option>
+                </select>
+              </div>
+              <div className="mb-6">
+                <p>
+                  Dokter{" "}
+                  <span className="font-medium text-gray-900">
+                    [NAMA DOKTER]
+                  </span>{" "}
+                  melakukan pada hari [HARI], [HARI], dan [HARI]
+                </p>
+              </div>
+              <div className="grid grid-cols-2 mb-6">
+                <div>
+                  <label
+                    htmlFor="date"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Pilih Tanggal
+                  </label>
+                  <div ref={flatpickrRef}></div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="time"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Pilih Waktu
+                  </label>
+                </div>
               </div>
             </div>
-          </div>
+          )}
+
+          {currentStep === 1 && (
+            <div className="grid">
+              <div className="mb-6 text-lg font-medium text-gray-900">
+                Pilih Pasien
+                <hr className="h-px bg-gray-700 border-0"></hr>
+              </div>
+              <div className="mb-6">
+                <label
+                  htmlFor="gender"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Pilih Profil Pasien
+                </label>
+                <select
+                  id="gender"
+                  className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  required
+                >
+                  <option>Pilih Profil Pasien</option>
+                  <option value="0">
+                    Budi Santoso (0x1234567890123456789012345678901234567890)
+                  </option>
+                  <option value="1">
+                    Alif Maulidanar (0x66E167fDd23614b58A4459C1C875C6705f550ED6)
+                  </option>
+                  <option value="2">
+                    Citra Indriani (0x9876543210987654321098765432109876543210)
+                  </option>
+                </select>
+              </div>
+              <div className="mb-6">
+                <p>
+                  Belum memiliki profil pasien? Silakan melakukan pendaftaran
+                  pasien terlebih dahulu.
+                </p>
+              </div>
+            </div>
+          )}
+
+          {currentStep === 2 && (
+            <div className="grid">
+              <div className="mb-6 text-lg font-medium text-gray-900">
+                Konfirmasi Appointment
+                <hr className="h-px bg-gray-700 border-0"></hr>
+              </div>
+              <div className="mb-6">
+                <label
+                  htmlFor="gender"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Pilih Rumah Sakit
+                </label>
+                <select
+                  id="gender"
+                  className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  required
+                >
+                  <option>Pilih Rumah Sakit</option>
+                  <option value="0">Jakarta</option>
+                  <option value="1">Bekasi</option>
+                  <option value="2">Tangerang</option>
+                </select>
+              </div>
+            </div>
+          )}
 
           <div className="grid grid-cols-2 mt-8 text-center gap-x-4">
-            <button
-              type="button"
-              onClick={handleCancel}
-              className="text-white bg-red-700 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-fit sm:w-auto px-5 py-2.5 text-center"
-            >
-              Batal
-            </button>
-            <button
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-fit sm:w-auto px-5 py-2.5 text-center"
-            >
-              Selanjutnya
-            </button>
+            {currentStep === 0 && (
+              <button
+                type="button"
+                onClick={handleCancel}
+                className="text-white bg-red-700 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-fit sm:w-auto px-5 py-2.5 text-center"
+              >
+                Batal
+              </button>
+            )}
+            {currentStep > 0 && (
+              <button
+                type="button"
+                onClick={handleBack}
+                className="text-white bg-red-700 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-fit sm:w-auto px-5 py-2.5 text-center"
+              >
+                Kembali
+              </button>
+            )}
+            {currentStep < 2 ? (
+              <button
+                type="button"
+                onClick={handleNext}
+                className="text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-fit sm:w-auto px-5 py-2.5 text-center"
+              >
+                Selanjutnya
+              </button>
+            ) : (
+              <button
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-fit sm:w-auto px-5 py-2.5 text-center"
+              >
+                Buat Appointment
+              </button>
+            )}
           </div>
         </form>
       </Modal>
