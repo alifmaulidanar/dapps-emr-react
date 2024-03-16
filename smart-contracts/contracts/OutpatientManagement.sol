adc069e39e971df316241c16f661fe7c1282e1c3
alifmaulidanar <alifmaulidanr@gmail.com>
2024-03-17 00:39:46 +0700

    Update data structure for outpatient data, also prepare update for patient data structure in the future

--- smart-contracts/contracts/OutpatientManagement.sol
+++ smart-contracts/contracts/OutpatientManagement.sol
@@ -2,60 +2,77 @@
 pragma solidity ^0.8.22;
 
 contract OutpatientManagement {
-  event OutpatientDataAdded(uint id, address owner, address doctor, address nurse, string cid, uint createdAt);
-  struct OutpatientData {uint id; address patientAddress; string patientProfileId; address doctor; address nurse; string cid; uint createdAt; }
+    event OutpatientDataAdded(uint id, address owner, address doctor, address nurse, string cid);
+    event OutpatientDataUpdated(uint id, address owner, address doctor, address nurse, string cid);
 
-  mapping(address => uint[]) private appointmentsByPatient;
-  mapping(address => uint[]) private appointmentsByDoctor;
-  mapping(address => uint[]) private appointmentsByNurse;
-  uint private outpatientDataCounter = 1;
-  OutpatientData[] private outpatientData;
+    struct AppointmentData {uint id; address owner; string cid;}
+    
+    mapping(address => uint[]) private appointmentsByPatient;
+    mapping(address => uint[]) private appointmentsByDoctor;
+    mapping(address => uint[]) private appointmentsByNurse;
 
-  function addOutpatientData(address _patientAddress, string memory _patientProfileId, address _doctor, address _nurse, string memory _cid) external {
-      // Validasi input dan pastikan bahwa _patientAddress, _doctor, dan _nurse adalah valid
-      OutpatientData memory newOutpatientData = OutpatientData(
-          outpatientDataCounter++,
-          _patientAddress,
-          _patientProfileId,
-          _doctor,
-          _nurse,
-          _cid,
-          block.timestamp
-      );
-      outpatientData.push(newOutpatientData);
-      appointmentsByPatient[_patientAddress].push(newOutpatientData.id);
-      appointmentsByDoctor[_doctor].push(newOutpatientData.id);
-      appointmentsByNurse[_nurse].push(newOutpatientData.id);
-  }
+    uint private appointmentCounter = 1;
+    AppointmentData[] private patientAppointments;
+    AppointmentData[] private doctorAppointments;
+    AppointmentData[] private nurseAppointments;
 
-  function getAllAppointments() external view returns (OutpatientData[] memory) {
-      return outpatientData;
-  }
+    function addOutpatientData(address _patientAddress, address _doctorAddress, address _nurseAddress, string memory _cid) external {
+        uint patientAppointmentId = appointmentCounter;
+        uint doctorAppointmentId = appointmentCounter;
+        uint nurseAppointmentId = appointmentCounter;
+        appointmentCounter++;
 
-  function getAppointmentsByPatient(address _patientAddress) external view returns (OutpatientData[] memory) {
-      uint[] memory appointmentIds = appointmentsByPatient[_patientAddress];
-      OutpatientData[] memory appointments = new OutpatientData[](appointmentIds.length);
-      for (uint i = 0; i < appointmentIds.length; i++) {
-          appointments[i] = outpatientData[appointmentIds[i] - 1];
-      }
-      return appointments;
-  }
+        patientAppointments.push(AppointmentData(patientAppointmentId, _patientAddress, _cid));
+        doctorAppointments.push(AppointmentData(doctorAppointmentId, _doctorAddress, _cid));
+        nurseAppointments.push(AppointmentData(nurseAppointmentId, _nurseAddress, _cid));
 
-  function getAppointmentsByNurse(address _nurseAddress) external view returns (OutpatientData[] memory) {
-      uint[] memory appointmentIds = appointmentsByNurse[_nurseAddress];
-      OutpatientData[] memory appointments = new OutpatientData[](appointmentIds.length);
-      for (uint i = 0; i < appointmentIds.length; i++) {
-          appointments[i] = outpatientData[appointmentIds[i] - 1];
-      }
-      return appointments;
-  }
+        appointmentsByPatient[_patientAddress].push(patientAppointmentId);
+        appointmentsByDoctor[_doctorAddress].push(doctorAppointmentId);
+        appointmentsByNurse[_nurseAddress].push(nurseAppointmentId);
+    }
 
-  function getAppointmentsByDoctor(address _doctorAddress) external view returns (OutpatientData[] memory) {
-      uint[] memory appointmentIds = appointmentsByDoctor[_doctorAddress];
-      OutpatientData[] memory appointments = new OutpatientData[](appointmentIds.length);
-      for (uint i = 0; i < appointmentIds.length; i++) {
-          appointments[i] = outpatientData[appointmentIds[i] - 1];
-      }
-      return appointments;
-  }
+    function updateOutpatientData(address _patientAddress, string memory _cid) external {
+        require(appointmentsByPatient[_patientAddress].length > 0, "Appointments:404");
+        for (uint i = 0; i < appointmentsByPatient[_patientAddress].length; i++) {
+            uint appointmentId = appointmentsByPatient[_patientAddress][i];
+            AppointmentData storage patientAppointment = patientAppointments[appointmentId - 1];
+            AppointmentData storage doctorAppointment = doctorAppointments[appointmentId - 1];
+            AppointmentData storage nurseAppointment = nurseAppointments[appointmentId - 1];
+            patientAppointment.cid = _cid;
+            doctorAppointment.cid = _cid;
+            nurseAppointment.cid = _cid;
+            emit OutpatientDataUpdated(appointmentId, _patientAddress, doctorAppointment.owner, nurseAppointment.owner, _cid);
+        }
+    }
+
+    function getAllAppointments() external view returns (AppointmentData[] memory, AppointmentData[] memory, AppointmentData[] memory) {
+        return (patientAppointments, doctorAppointments, nurseAppointments);
+    }
+
+    function getAppointmentsByPatient(address _patientAddress) external view returns (AppointmentData[] memory) {
+        uint[] memory appointmentIds = appointmentsByPatient[_patientAddress];
+        AppointmentData[] memory appointments = new AppointmentData[](appointmentIds.length);
+        for (uint i = 0; i < appointmentIds.length; i++) {
+            appointments[i] = patientAppointments[appointmentIds[i] - 1];
+        }
+        return appointments;
+    }
+
+    function getAppointmentsByDoctor(address _doctorAddress) external view returns (AppointmentData[] memory) {
+        uint[] memory appointmentIds = appointmentsByDoctor[_doctorAddress];
+        AppointmentData[] memory appointments = new AppointmentData[](appointmentIds.length);
+        for (uint i = 0; i < appointmentIds.length; i++) {
+            appointments[i] = doctorAppointments[appointmentIds[i] - 1];
+        }
+        return appointments;
+    }
+
+    function getAppointmentsByNurse(address _nurseAddress) external view returns (AppointmentData[] memory) {
+        uint[] memory appointmentIds = appointmentsByNurse[_nurseAddress];
+        AppointmentData[] memory appointments = new AppointmentData[](appointmentIds.length);
+        for (uint i = 0; i < appointmentIds.length; i++) {
+            appointments[i] = nurseAppointments[appointmentIds[i] - 1];
+        }
+        return appointments;
+    }
 }
