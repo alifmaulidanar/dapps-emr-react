896c587a247c8331421f407ccf91fd0894b205ae
alifmaulidanar <alifmaulidanr@gmail.com>
2024-05-28 23:16:28 +0700

    Update doctor schedules

--- frontend/src/pages/admin/DoctorSchedule.jsx
+++ frontend/src/pages/admin/DoctorSchedule.jsx
@@ -6,15 +6,40 @@ import { Button, message, Upload, Tag } from "antd";
 import { Table } from "antd";
 import { CONN } from "../../../../enum-global";
 
-// Nurse tag color
-const getTagColor = (address) => {
+// Doctor tag color
+const getTagColorDoctor = (address) => {
   const colorToAddress = {
-    "0x7837Eb4C4388842335f8d51De5C003f2a5c42169": "red",
-    "0xBec10133688B118Bd183B73B7963617472800E83": "green",
-    "0x0be909d68efd2b18111254782D0Edf077CD7F6aD": "blue",
+    "0xf7C9Bd049Cc6e4538033AEa5254136F1DF9A4A6D": "red",
+    "0x0be909d68efd2b18111254782D0Edf077CD7F6aD": "green",
+    "0x119d6536434c434fa68CFFAFE104686a50E4Ef56": "blue",
+    "0x76bB4cb8050CFc0635E007B84C6D01b25c80326e": "orange",
+    "0xd43F2f3615B6F0ad5C8caC2f514Ae53F0F21e93C": "purple",
+    "0x5b5ef7fb6747e685D9aB23Aba8af1593283c1366": "yellow",
   };
-  return colorToAddress[address];
+  return colorToAddress[address] || "gray";
 };
+
+// Nurse tag color
+const getTagColorNurse = (address) => {
+  const colorToAddress = {
+    "0x64cEe3E0CD7E59d1eD387717e41D8ac6ca291522": "red",
+    "0xadc8158B4d4b63126C18B258BAFd94d7e4614e58": "green",
+    "0x2d761572fb2962d9a5c4D6DF34b120947bb3AbC1": "blue",
+    "0xe5dF08799114D618e628e2027fc8FF5B7F29705C": "orange",
+    "0x7837Eb4C4388842335f8d51De5C003f2a5c42169": "purple",
+    "0xBec10133688B118Bd183B73B7963617472800E83": "teal",
+    "0x6b32DFFB8d087297b7397adE4F94AC06e1D47cA6": "brown",
+    "0x2B8B2e67E1176378304597B044D766580D233e1f": "pink",
+    "0xC1Df9ad321ce0da253e32d1e21ea11BfD27Da498": "yellow",
+    "0xBE9EED2EfB39F5E80555e7200de80E5b145E66f6": "cyan",
+    "0xd43F2f3615B6F0ad5C8caC2f514Ae53F0F21e93C": "magenta",
+    "0xdD76Ce44d1A22D5a3bCeD70e766AAAC95dc06f1b": "lime",
+    "0xd078d842A58Df0A2282Ea626ce0629Af1C542A84": "navy",
+    "0x119d6536434c434fa68CFFAFE104686a50E4Ef56": "maroon",
+  };
+  return colorToAddress[address] || "gray";
+};
+
 
 function DoctorSchedule({ schedulesData, onScheduleCidUpdate }) {
   const [spinning, setSpinning] = React.useState(false);
@@ -29,18 +54,33 @@ function DoctorSchedule({ schedulesData, onScheduleCidUpdate }) {
       const { dokter } = await ipfsResponse.json();
       if (!Array.isArray(dokter)) throw new Error("Data format is incorrect");
       let flattenedData = [];
+      console.log({ dokter });
       dokter.forEach((dokter) => {
         dokter.jadwal.forEach((schedule, index) => {
           flattenedData.push({
             key: dokter.idDokter,
-            accountAddressDoctor: dokter.accountAddressDoctor,
-            namaDokter: dokter.namaDokter,
+            namaDokter: (
+              <>
+                <p>{dokter.namaDokter}</p>
+                <Tag color={getTagColorDoctor(dokter.doctorAddress)}>
+                  {`${dokter.doctorAddress}`}
+                </Tag>
+              </>
+            ),
+            // namaDokter: `${dokter.namaDokter}\n${dokter.doctorAddress}`,
             spesialisasiDokter: dokter.spesialisasiDokter,
             lokasiPraktik: dokter.lokasiPraktik,
             hari: schedule.hari,
             waktu: schedule.waktu,
-            accountAddressNurse: schedule.accountAddressNurse,
-            namaPerawat: schedule.namaPerawat,
+            namaAsisten: (
+              <div>
+                <p>{schedule.namaAsisten}</p>
+                <Tag color={getTagColorNurse(schedule.nurseAddress)}>
+                  {`${schedule.nurseAddress}`}
+                </Tag>
+              </div>
+            ),
+            jabatanAsisten: schedule.jabatanAsisten,
             rowSpan: index === 0 ? dokter.jadwal.length : 0,
           });
         });
@@ -71,21 +111,9 @@ function DoctorSchedule({ schedulesData, onScheduleCidUpdate }) {
       },
     },
     {
-      title: "Nama",
+      title: "Nama Dokter",
       dataIndex: "namaDokter",
       key: "namaDokter",
-      render: (value, row, index) => {
-        const obj = {
-          children: value,
-          props: { rowSpan: row.rowSpan },
-        };
-        return obj;
-      },
-    },
-    {
-      title: "Alamat Dokter",
-      dataIndex: "accountAddressDoctor",
-      key: "accountAddressDoctor",
       render: (value, row, index) => {
         const obj = {
           children: value,
@@ -129,16 +157,21 @@ function DoctorSchedule({ schedulesData, onScheduleCidUpdate }) {
       key: "waktu",
     },
     {
-      title: "Perawat",
-      dataIndex: "namaPerawat",
-      key: "namaPerawat",
+      title: "Nama Asisten",
+      dataIndex: "namaAsisten",
+      key: "namaAsisten",
     },
     {
-      title: "Perawat",
-      dataIndex: "accountAddressNurse",
-      key: "accountAddressNurse",
-      render: (nurse) => <Tag color={getTagColor(nurse)}>{nurse}</Tag>,
+      title: "Jabatan Asisten",
+      dataIndex: "jabatanAsisten",
+      key: "jabatanAsisten",
     },
+    // {
+    //   title: "Perawat",
+    //   dataIndex: "nurseAddress",
+    //   key: "nurseAddress",
+    //   render: (nurse) => <Tag color={getTagColorNurse(nurse)}>{nurse}</Tag>,
+    // },
   ];
 
   // handle upload
@@ -189,6 +222,7 @@ function DoctorSchedule({ schedulesData, onScheduleCidUpdate }) {
         rowKey="key"
         loading={spinning}
         size="middle"
+        pagination={false}
       />
     </div>
   );
