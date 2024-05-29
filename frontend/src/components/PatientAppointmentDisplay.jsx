414ffe2765dbde76cceb0704bdf155c26baaf057
alifmaulidanar <alifmaulidanr@gmail.com>
2024-05-29 12:36:10 +0700

    Adjustment of the code so that the details of the patient's appointment registration can be displayed perfectly

--- frontend/src/components/PatientAppointmentDisplay.jsx
+++ frontend/src/components/PatientAppointmentDisplay.jsx
@@ -21,7 +21,7 @@ const PatientRecordLoop = ({ data }) => {
 
 function PatientAppointmentDisplay({ data, token }) {
   const [selectedAccount, setSelectedAccount] = useState(null);
-  const [appointmentStatus, setAppointmentStatus] = useState(data.appointment.data.status);
+  const [appointmentStatus, setAppointmentStatus] = useState(data.appointment.status);
   const getSigner = useCallback(async () => {
     const win = window;
     if (!win.ethereum) { console.error("Metamask not detected"); return; }
@@ -49,26 +49,27 @@ function PatientAppointmentDisplay({ data, token }) {
   }, []);
 
   const patientDataProps1 = [
-    { key: "rumahSakit", value1: "Cabang Eka Hospital", value2: (<p>{data.appointment.data.rumahSakit}</p>), },
-    { key: "tanggalTerpilih", value1: "Hari & Tanggal", value2: (<p>{new Date(data.appointment.data.tanggalTerpilih).toLocaleDateString('id-ID', { weekday: 'long', day: '2-digit', month: 'long', year: 'numeric' })}</p>), },
-    { key: "emrNumber", value1: "Nomor Rekam Medis", value2: (<p>{data.appointment.data.emrNumber}</p>), },
-    { key: "waktuTerpilih", value1: "Waktu Praktik", value2: (<p>{data.appointment.data.waktuTerpilih}</p>), },
+    // { key: "faskesTujuan", value1: "Faskes Tujuan", value2: (<p>{data.appointment.faskesTujuan}</p>), },
+    { key: "tanggalTerpilih", value1: "Hari & Tanggal Berobat", value2: (<p>{new Date(data.appointment.tanggalTerpilih).toLocaleDateString('id-ID', { weekday: 'long', day: '2-digit', month: 'long', year: 'numeric' })}</p>), },
+    { key: "waktuTerpilih", value1: "Waktu Praktik", value2: (<p>{data.appointment.waktuTerpilih}</p>), },
+    { key: "emrNumber", value1: "Nomor Rekam Medis", value2: (<p>{data.appointment.emrNumber}</p>), },
+    { key: "nomorIdentitas", value1: "Nomor Identitas Pasien (NIK/SIM/Paspor)", value2: (<p>{data.appointment.nomorIdentitas}</p>), },
   ];
   const patientDataProps2 = [
-    { key: "namaDokter", value1: "Nama Dokter", value2: (<p>{data.appointment.data.namaDokter}</p>), },
-    { key: "doctorAddress", value1: "Alamat Akun Dokter", value2: (<Tag color="gold" className="m-0">{data.appointment.data.doctorAddress}</Tag>), },
-    { key: "namaAsisten", value1: "Nama Perawat", value2: (<p>{data.appointment.data.namaAsisten}</p>), },
-    { key: "nurseAddress", value1: "Alamat Akun Perawat", value2: (<Tag color="gold" className="m-0">{data.appointment.data.nurseAddress}</Tag>), },
-    { key: "namaLengkap", value1: "Nama Pasien", value2: (<p>{data.appointment.data.namaLengkap}</p>), },
-    { key: "nomorIdentitas", value1: "Nomor Identitas Pasien (NIK/SIM/Paspor)", value2: (<p>{data.appointment.data.nomorIdentitas}</p>), },
-    { key: "email", value1: "Email Pasien", value2: (<p>{data.appointment.data.email}</p>), },
-    { key: "telpSelular", value1: "Nomor Telepon Pasien", value2: (<p>{data.appointment.data.telpSelular}</p>), },
-    { key: "createdAt", value1: "Pendaftaran Dibuat Pada", value2: (<p>{new Date(data.appointment.data.createdAt).toLocaleDateString('id-ID', { weekday: 'long', day: '2-digit', month: 'long', year: 'numeric' })}</p>), },
+    { key: "namaLengkap", value1: "Nama Pasien", value2: (<p>{data.appointment.namaLengkap}</p>), },
+    { key: "accountAddress", value1: "Alamat Akun Pasien", value2: (<Tag color="green" className="m-0">{data.appointment.accountAddress}</Tag>), },
+    { key: "email", value1: "Email Pasien", value2: (<p>{data.appointment.email || "-"}</p>), },
+    { key: "telpSelular", value1: "Nomor Telepon Pasien", value2: (<p>{data.appointment.telpSelular || "-"}</p>), },
+    { key: "namaDokter", value1: "Nama Dokter", value2: (<p>{data.appointment.namaDokter}</p>), },
+    { key: "doctorAddress", value1: "Alamat Akun Dokter", value2: (<Tag color="gold" className="m-0">{data.appointment.doctorAddress}</Tag>), },
+    { key: "namaAsisten", value1: "Nama Perawat", value2: (<p>{data.appointment.namaAsisten}</p>), },
+    { key: "nurseAddress", value1: "Alamat Akun Perawat", value2: (<Tag color="gold" className="m-0">{data.appointment.nurseAddress}</Tag>), },
+    { key: "createdAt", value1: "Pendaftaran Dibuat Pada", value2: (<p>{new Date(data.appointment.appointmentCreatedAt).toLocaleDateString('id-ID', { weekday: 'long', day: '2-digit', month: 'long', year: 'numeric' })}</p>), },
     { key: "patientGender", value1: "Status Rawat Jalan", value2: (<Tag color={ appointmentStatus === "ongoing" ? "blue" : appointmentStatus === "done" ? "green" : "red" }>{ appointmentStatus === "ongoing" ? "Sedang berjalan" : appointmentStatus === "done" ? "Selesai" : "Batal" }</Tag>), },
   ];
 
   const cancelAppointment = async () => {
-    const signedData = { emrNumber: data.appointment.data.emrNumber, appointmentId: data.appointment.data.appointmentId }
+    const signedData = { emrNumber: data.appointment.emrNumber, appointmentId: data.appointment.appointmentId }
     const signer = await getSigner();
     const signature = await signer.signMessage(JSON.stringify(signedData));
     signedData.signature = signature;
@@ -98,14 +99,16 @@ function PatientAppointmentDisplay({ data, token }) {
     <div className="grid px-12 py-8 gap-y-8">
       <div className="grid items-center mx-auto text-center text-gray-900 w-fit gap-y-2">
         <h1 className="text-xl font-medium">Data Pendaftaran Rawat Jalan</h1>
-        <p>ID Pendaftaran: <Tag color="green" className="m-0">{data.appointment.data.appointmentId}</Tag></p>
+        {/* <p>ID Pendaftaran: <Tag color="green" className="m-0">{data.appointment.appointmentId}</Tag></p> */}
+        <p>ID Pendaftaran: <span className="font-semibold">{data.appointment.appointmentId}</span></p>
+        <p>{data.appointment.faskesTujuan}</p>
       </div>
       <div className="grid grid-cols-2 gap-x-8 gap-y-6">
         <PatientRecordLoop data={patientDataProps1} />
         <PatientRecordLoop data={patientDataProps2} />
       </div>
       <div className="justify-center w-1/3 mx-auto">
-      {data.appointment.data.status === "ongoing" ? <Button type="primary" danger onClick={cancelAppointment}>Batalkan Pendaftaran</Button> : null}
+      {data.appointment.status === "ongoing" ? <Button type="primary" danger onClick={cancelAppointment}>Batalkan Pendaftaran</Button> : null}
       </div>
     </div>
   );
