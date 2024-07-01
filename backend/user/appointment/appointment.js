a5dce3883ccc7c8baec4c9ffc62090c5b9ad7b87
alifmaulidanar <alifmaulidanr@gmail.com>
2024-07-01 13:56:57 +0700

    Feature - Add endpoint to get outpatient queues

--- backend/user/appointment/appointment.js
+++ backend/user/appointment/appointment.js
@@ -68,6 +68,69 @@ export const handleFileWrite = (specialization, selectedDate) => {
   return queueNumber;
 };
 
+// GET Queue
+router.post("/:role/queue", authMiddleware, async (req, res) => {
+  try {
+    const { selectedDate } = req.body;
+    const basePath = path.join(__dirname, '../../patient/services', selectedDate);
+
+    const readLines = (filePath) => {
+      return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+          if (err) {
+            if (err.code === 'ENOENT') {
+              return resolve([]);
+            }
+            return reject(err);
+          }
+          const lines = data.trim().split('\n');
+          resolve(lines);
+        });
+      });
+    };
+
+    const kiaPath = path.join(basePath, 'kia.txt');
+    const tbparuPath = path.join(basePath, 'tbparu.txt');
+    const umumPath = path.join(basePath, 'umum.txt');
+
+    if (!fs.existsSync(basePath)) {
+      console.log(`Folder for date ${selectedDate} not found.`);
+      return res.status(200).json({ 
+        message: "Queue fetched successfully",
+        lastLines: {
+          kia: [],
+          tbparu: [],
+          umum: []
+        }
+      });
+    }
+
+    const [linesKia, linesTbparu, linesUmum] = await Promise.all([
+      readLines(kiaPath),
+      readLines(tbparuPath),
+      readLines(umumPath)
+    ]);
+
+    // console.log(`=====================================================`);
+    // console.log(`Data antrean tanggal ${selectedDate}`)
+    // console.log(`Lines from kia.txt: ${linesKia}`);
+    // console.log(`Lines from tbparu.txt: ${linesTbparu}`);
+    // console.log(`Lines from umum.txt: ${linesUmum}`);
+
+    res.status(200).json({ 
+      message: "Queue fetched successfully",
+      queues: {
+        kia: linesKia,
+        tbparu: linesTbparu,
+        umum: linesUmum
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
 // GET Doctor Schedule
 router.get("/:role/appointment", authMiddleware, async (req, res) => {
   try {
